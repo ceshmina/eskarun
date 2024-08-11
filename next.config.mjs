7a783dd0e5a0f61236a3d5d8617f0efb3db7483d
ceshmina <shu@apkas.net>
2024-08-11 21:45:07 +0900

    ビルド用のdocker compose設定を追加

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: process.env.NODE_ENV === 'production' ? 'export' : undefined
+}
 
 export default nextConfig

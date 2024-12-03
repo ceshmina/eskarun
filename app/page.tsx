603cd0dde4f3703f5834d7a2c690822f1fe03746
ceshmina <shu@apkas.net>
2024-12-03 23:06:26 +0900

    最新の日記5件を表示

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { getArticles } from '@/core/articles'
 import ArticleCard from '@/components/card'
 
 export default async function Page() {
-  const articles = (await getArticles()).slice(0, 30)
+  const articles = (await getArticles()).slice(0, 5)
   return (
     <main className="max-w-screen-md mx-auto p-4">
       <div className="my-4">
@@ -10,9 +10,12 @@ export default async function Page() {
         <p className="text-base font-normal">shu&apos;s diary</p>
       </div>
 
-      {articles.map((article) => (
-        <ArticleCard key={article.slug} article={article} />
-      ))}
+      <div className="my-16">
+        <p className="my-2 text-lg font-bold">最新の日記</p>
+        {articles.map((article) => (
+          <ArticleCard key={article.slug} article={article} />
+        ))}
+      </div>
     </main>
   )
 }

17178f9b9765a166080a0a74de70c38d01706302
ceshmina <shu@apkas.net>
2024-12-13 23:24:08 +0900

    年月をクリックで開閉

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
-import { parse, format } from 'date-fns'
 import { aggArticlesByMonth, aggArticlesByCamera, aggArticlesByLocation } from '@/core/aggregate'
+import Year from '@/components/navigation/year'
 
 export default async function Navigation() {
   const articlesByMonth = await aggArticlesByMonth()
@@ -11,20 +11,9 @@ export default async function Navigation() {
       <div>
         <h2 className="text-lg font-bold">月別</h2>
         <ul className="my-4">
-          {articlesByMonth.map(({ year, months }) => {
-            const formatYear = format(parse(year, 'yyyy', new Date()), 'yyyy年')
-            return (<li key={year} className="my-1 text-sm font-bold">
-              {formatYear}
-              <ul className="mt-2 mb-4 ml-4">
-                {months.map(({ month, count }) => {
-                  const formatMonth = format(parse(month, 'yyyyMM', new Date()), 'yyyy年M月')
-                  return (<li key={month} className="my-1 text-sm font-normal">
-                    <Link href={`/months/${month}`} className="text-blue-300">{formatMonth} ({count})</Link>
-                  </li>)
-                })}
-              </ul>
-            </li>)
-          })}
+          {articlesByMonth.map(({ year, months }) => (
+            <Year key={year} year={year} months={months} />
+          ))}
         </ul>
       </div>
 
---
+++ components/navigation/year.tsx
@@ -0,0 +1,38 @@
+'use client'
+import { useState, MouseEvent } from 'react'
+import Link from 'next/link'
+import { GoTriangleDown, GoTriangleUp } from 'react-icons/go'
+import { parse, format } from 'date-fns'
+
+type Props = Readonly<{
+  year: string
+  months: ReadonlyArray<{ month: string, count: number }>
+}>
+
+export default function Year({ year, months }: Props) {
+  const formatYear = format(parse(year, 'yyyy', new Date()), 'yyyy年')
+  const countSum = months.reduce((acc, { count }) => acc + count, 0)
+
+  const [isOpen, setIsOpen] = useState(false)
+  const toggleOpen = (event: MouseEvent) => {
+    event.stopPropagation()
+    setIsOpen(!isOpen)
+  }
+
+  return (<li key={year} className="my-1 text-sm font-bold">
+    <p className="mb-2" onClick={toggleOpen}>
+      {formatYear} ({countSum}) {isOpen ?
+        <GoTriangleUp className="text-lg inline-block" /> :
+        <GoTriangleDown className="text-lg inline-block" />
+      }
+    </p>
+    {isOpen && <ul className="mt-2 mb-4 ml-4">
+      {months.map(({ month, count }) => {
+        const formatMonth = format(parse(month, 'yyyyMM', new Date()), 'yyyy年M月')
+        return (<li key={month} className="my-1 text-sm font-normal">
+          <Link href={`/months/${month}`} className="text-blue-300">{formatMonth} ({count})</Link>
+        </li>)
+      })}
+    </ul>}
+  </li>)
+}
